ffad15c15cc5afe7d241d5fd9a8b88769646e737
Murad Hasil <160184391+Murad-Hasil@users.noreply.github.com>
2025-10-20 23:26:09 +0500

    updated favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,17 +67,12 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/icons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/icons/favicon-24x24.png", sizes: "24x24", type: "image/png" },
       { url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      {
-        url: "/icons/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        url: "/icons/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
+      { url: "/icons/favicon-64x64.png", sizes: "64x64", type: "image/png" },
+      { url: "/icons/favicon-128x128.png", sizes: "128x128", type: "image/png" },
+      { url: "/icons/favicon-256x256.png", sizes: "256x256", type: "image/png" },
+      { url: "/icons/favicon-512x512.png", sizes: "512x512", type: "image/png" },
     ],
     apple: [
       {
